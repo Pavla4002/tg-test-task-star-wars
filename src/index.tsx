c33344b4180f4988d5ed3f62e2edfef4705835ba
Павла <paha16kot@inbox.ru>
2025-04-08 10:12:16 +0300

    Исправледние багов 3.0

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BrowserRouter, Outlet} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import Router from "@/app/routes/Router";
 
 const queryClient = new QueryClient({
