f278be1b083a837a70a9263e0672099e0f5cf9ee
Павла <paha16kot@inbox.ru>
2025-04-08 10:20:02 +0300

    Исправледние багов 4.0

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div className={styles.loader}>
-            <h4>Загрузка данных... <br/>
-                Ожидайте, пожалуйста :)
-            </h4>
+        <div className={styles.loaderBox}>
+            <div className={styles.loader}>
+                <h4>Загрузка данных... <br/>
+                    Ожидайте, пожалуйста :)
+                </h4>
+            </div>
         </div>
+
     );
 }
 
